542f76bf0428e8ad87d5eff8f4bac7d17a24f906
ghofranefatnassi <ghofranefatnassi850@gmil.com>
2024-12-20 23:26:14 +0100

    last commite

--- djangoMLDeployment/irisApp/views.py
+++ djangoMLDeployment/irisApp/views.py
@@ -5,10 +5,10 @@ model = load('./savedModels/model.joblib')
 
 def predictor(request):
     if request.method == 'POST':
-        sepal_length = request.POST['sepal_length']
-        sepal_width = request.POST['sepal_width']
-        petal_length = request.POST['petal_length']
-        petal_width = request.POST['petal_width']
+        sepal_length = float(request.POST['sepal_length'])
+        sepal_width = float(request.POST['sepal_width'])
+        petal_length = float(request.POST['petal_length'])
+        petal_width = float(request.POST['petal_width'])
         y_pred = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
         if y_pred[0] == 0:
             y_pred = 'Setosa'
